3744daf38de6ea52eb9025ce3dd9be528b4f6120
GianCHerrera <83032013+GianCHerrera@users.noreply.github.com>
2021-10-11 11:56:35 -0500

    Perfil 40%
    Se crea el archivo de perfil y se comienza a crear el diseño falta funcionalidad

---
+++ common/perfil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <title>Perfil</title>
+</head>
+
+<body>
+    <div class="flexperfil">
+        <dic class="gridperfil">
+            <div class="img-perfil">
+                <div class="opacidad">
+                    <div class="fotoperfil">
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="contenedor-perfil">
+                <p>usuario</p>
+            </div>
+        </dic>
+    </div>
+
+</body>
+
+</html>
