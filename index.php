099a286c74c5fe7eff1a69b363ba71ccdb58755b
miklegonza <miklegonza1803@outlook.com>
2021-10-15 00:24:32 -0500

    Diseño de la ventana home

--- index.php
+++ index.php
@@ -3,29 +3,36 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/css/style_home.css">
-    <link rel="icon" href="./img/icon.png">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style_home.css">
+    <link rel="icon" href="./img/iconNoBackground.png">
     <title>Hack3rs L4b</title>
+    <script>
+        setInterval(function() {
+            location.reload()
+        }, 35000)
+    </script>
 </head>
 
 <body>
 
-    <header>
-        <div class="header">
-            <div class="logo">
-                <a href="#" class="logoYtexto">
-                    <img class="logoimagen" src="/img/iconNoBackground.png" alt="">
-                    <h1 class="nombre">Hack3rs <span>L4b</span></h1>
-                </a>
-            </div>
+    <nav class="nav-bar">
+        <ul>
+            <a href="./common/inicio-sesion.php"><li>Iniciar sesión</li></a>
+            <a href="./common/dashboard.php"><li>Continuar</li></a>
+        </ul>
+    </nav>
 
-            <div class="navegacion-principal">
-                <a href="#">Nosotros</a>
-                <a href="#">Regístrate</a>
-                <a href="./common/inicio-sesion.php">Inciar Sesión</a>
-            </div>
+    <div class="container">
+        <div class="content">
+            <h1 class="title nombre">Hack3rs <span>L4b</span></h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id quos autem accusamus
+                nostrum sit nesciunt, magnam minima doloribus libero repudiandae dolore debitis
+                consectetur fugiat, accusantium architecto officia tempore molestiae similique
+            </p>
         </div>
-    </header>
+    </div>
 
 </body>
 
